bae83b39c41a8d4b2665dea1d0259a66b4e2d26c
naoya kurihara <kuriharanaoya@gmail.com>
2016-04-05 22:21:31 +0900

    Delete lazer event handler

--- Assets/Scripts/ILazerHitEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.EventSystems;
-
-public interface ILazerHitEvent : IEventSystemHandler
-{
-    void OnLazerHit(RaycastHit hitInfo);
-}
